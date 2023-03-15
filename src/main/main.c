182fea1c7f64263e53249146ce453fec11703f4c
Utku Çelik <43106673+HutkuC@users.noreply.github.com>
2023-03-15 19:51:32 +0300

    Fix input

--- src/main/main.c
+++ src/main/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "lib/math.h"
 
 int32_t main() {
@@ -6,6 +7,8 @@ int32_t main() {
         printf(">");
         char input[300];
         fgets(input, 300, stdin);
+        input[strlen(input) - 1] = '\0';
+        
         // TODO: evaluate input
     }
     return 0;
