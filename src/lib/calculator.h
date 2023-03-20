2ca488b679da29d81b220fb963b7ef956b350da6
Utku Çelik <43106673+HutkuC@users.noreply.github.com>
2023-03-21 02:11:20 +0300

    Add comments to calculator.h

--- src/lib/calculator.h
+++ src/lib/calculator.h
@@ -10,14 +10,40 @@
 #include "validator.h"
 #include "evaluator.h"
 
+/*
+* This function is the main function of the calculator.
+* It takes a string as input and evaluates it.
+* It also handles the assignment of variables.
+* @param input: The string to be evaluated.
+*/
 void calculator(char* input);
 
+/*
+* This function is used to find the assignment sign in the input string.
+* @param input: The string to be evaluated.
+* @return: The index of the assignment sign.
+*/
 int find_assignment_sign(char* input);
 
+/*
+* This function is used to remove the comments from the input string.
+* @param input: The string to be evaluated.
+*/
 void remove_comments(char* input);
 
+/*
+* This function is used to check if the input string is blank.
+* @param input: The string to be evaluated.
+* @return: True if the string is blank, false otherwise.
+*/
 bool isBlank(char* input);
 
+/*
+* This function is used to copy the variable name from the input string.
+* @param variable: The string to store the variable name.
+* @param input: The string to be evaluated.
+* @param size: The size of the variable name.
+*/
 void copyVariable(char* variable, char* input, int size);
 
 #endif
