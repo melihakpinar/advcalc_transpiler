b12db4ca80a3104d36f1365b8e93670297eb0cee
Utku Çelik <43106673+HutkuC@users.noreply.github.com>
2023-03-21 02:13:39 +0300

    Add comments to hashmap.h

--- src/lib/hashmap.h
+++ src/lib/hashmap.h
@@ -8,17 +8,42 @@
 #define P 61
 #define N 1000003
 
+/*
+* This struct is used to store the variables.
+*/
 typedef struct Hashmap{
     int64_t* container;
     int size;
 } hashmap;
 
+/*
+* This function is used to construct the hashmap.
+* @param size: The size of the hashmap.
+* @return: The constructed hashmap.
+*/
 hashmap* map_construct(int size);
 
+/*
+* This function is used to hash the variable name.
+* @param string: The variable name.
+* @return: The hash value of the variable name.
+*/
 int hash(char* string);
 
+/*
+* This function is used to assign a value to a variable.
+* @param map: The hashmap to store the variables.
+* @param variable: The variable name.
+* @param value: The value to be assigned.
+*/
 void map_assign(hashmap* map, char* variable, int64_t value);
 
+/*
+* This function is used to get the value of a variable.
+* @param map: The hashmap to store the variables.
+* @param variable: The variable name.
+* @return: The value of the variable.
+*/
 int64_t map_get(hashmap* map, char* variable);
 
 #endif
