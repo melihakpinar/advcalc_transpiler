a78026f378fd7ba93bf244cabb9ee49377e3a7d8
utkucelik <h.utku.c@gmail.com>
2023-04-01 22:45:21 +0300

    ctrl d handling

--- src/main/main.c
+++ src/main/main.c
@@ -3,12 +3,12 @@
 
 #include "lib/calculator.h"
 
-int32_t main() {
+int32_t main(void) {
     while(1){
         printf("> ");
         char input[300];
-        fgets(input, 300, stdin);
-        if(strlen(input) == 0)
+        char* tmp = fgets(input, 300, stdin);
+        if(tmp == NULL)
             break;
         input[strlen(input) - 1] = '\0';
         calculator(input);
