e3dac3a2834b19c3769aa9c18287ea49887b5f57
Utku Çelik <h.utku.c@gmail.com>
2023-04-30 01:28:56 +0300

    Initial commit

---
+++ src/lib/printer.c
@@ -0,0 +1,26 @@
+#include "printer.h"
+
+
+void write_begin(){
+    
+}
+
+void write_end(){
+
+}
+
+void allocate(char* variable){
+
+}
+
+void assign(char* variable, char* value){
+
+}
+
+void print(char* address){
+
+}
+
+void operation(char* address, char* left, char* right, char* op){
+
+}
---
+++ src/lib/printer.h
@@ -0,0 +1,17 @@
+#ifndef PRINTER_H
+#define PRINTER_H
+
+
+void write_begin();
+
+void write_end();
+
+void allocate(char* variable);
+
+void assign(char* variable, char* value);
+
+void print(char* address);
+
+void operation(char* address, char* left, char* right, char* op);
+
+#endif
